a565a3764cc0a8b6de5cdb0a2fc920b39dea19dc
kunst1080 <kontrapunkt1080@gmail.com>
2022-12-08 23:03:40 +0900

    topページのロードを待つ

--- src/top/main.tsx
+++ src/top/main.tsx
@@ -5,19 +5,51 @@ const click = (e: HTMLElement | null) => {
     } else {
         return false;
     }
-}
-window.onload = () => {
-    const columns = document.querySelectorAll<HTMLElement>("main > div[direction=column] > section[class]");
-    columns.forEach((c) => {
-        const ul = c.querySelector<HTMLElement>("ul[data-is-wide]");
-        if (ul) {
-            const arr = [];
-            do {
-                const liHtml = ul.innerHTML;
-                arr.push(liHtml);
-            } while (click(c.querySelector<HTMLElement>("[data-type=next]:not([data-hidden])")));
-            ul.innerHTML = arr.join('');
-            ul.querySelectorAll<HTMLElement>("span.indicator").forEach((e) => e.remove());
+};
+
+const sectionCallback = (section: HTMLElement, label: string) => {
+    const ul = section.querySelector<HTMLElement>("ul[data-is-wide]");
+    if (ul) {
+        const prevButton =
+            section.querySelector<HTMLElement>("[data-type=prev]");
+        const nextButton =
+            section.querySelector<HTMLElement>("[data-type=next]");
+        const arr = [];
+        do {
+            const liHtml = ul.innerHTML;
+            arr.push(liHtml);
+        } while (!nextButton?.hasAttribute("data-hidden") && click(nextButton));
+        ul.innerHTML = arr.join("");
+        ul.querySelectorAll<HTMLElement>("span.indicator").forEach((e) =>
+            e.remove()
+        );
+        prevButton?.setAttribute("data-hidden", "true");
+    }
+};
+
+const appObserver = new MutationObserver((mutationsList, observer) => {
+    const arr = new Set<HTMLElement>();
+    const filterAdd = (n: Node): void => {
+        const e = n as HTMLElement;
+        if (e.tagName == "SECTION") {
+            arr.add(e);
         }
+    };
+    mutationsList.forEach((m) => {
+        filterAdd(m.target);
+        m.addedNodes.forEach(filterAdd);
     });
-}
+    arr.forEach((s) => sectionCallback(s, "observer"));
+});
+appObserver.observe(document.getElementById("root") as Node, {
+    attributes: false,
+    childList: true,
+    subtree: true,
+});
+window.onload = (e) => {
+    document
+        .querySelectorAll<HTMLElement>(
+            "main > div[direction=column] > section[class]"
+        )
+        .forEach((s) => sectionCallback(s, "onload"));
+};
