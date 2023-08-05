a0978d3de6ccb6151676b3381f47c505d01b70b3
kunst1080 <kontrapunkt1080@gmail.com>
2023-08-05 11:39:29 +0900

    refactor: main function

--- src/bookmark/main.tsx
+++ src/bookmark/main.tsx
@@ -1,38 +1,45 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { nodeToBook } from "./Book";
+import { Book, nodeToBook } from "./model/Book";
 
 import { CashbackDetailComponent } from "./component/CashbackDetailComponent";
 import { Filter, FilterComponnet } from "./component/FilterComponent";
 
+// Filter
+const initializeFilter = (books: Book[]) => {
+    const handleFilter = (filter: Filter) => {
+        books.forEach((b) => {
+            const isShow =
+                b.title.includes(filter.title) &&
+                (!filter.discount || b.txtOff != null) &&
+                (!filter.cachback || b.isCashback) &&
+                (!filter.cachback2 || (b.isCashback && b.rate && b.rate >= 50));
+            b.ref.hidden = !isShow;
+        });
+    };
+    const filterApp = document.createElement("div");
+    filterApp.className = "d-lcol";
+    document
+        .querySelector("#main-bmk > div.d-area > div > div:last-of-type")
+        ?.prepend(filterApp);
+    ReactDOM.render(<FilterComponnet onUpdate={handleFilter} />, filterApp);
+};
+
+// CashbackDetail
+const initializeCacheDetila = (books: Book[]) => {
+    books
+        .filter((b) => b.isCashback)
+        .forEach((b) => {
+            const app = document.createElement("div");
+            b.ref.firstElementChild?.appendChild(app);
+            ReactDOM.render(<CashbackDetailComponent book={b} />, app);
+        });
+};
+
+// main
 const allItems = Array.from(document.querySelectorAll("ul#list > li")).map(
     nodeToBook
 );
-
-// Filter
-const handleFilter = (filter: Filter) => {
-    allItems.forEach((b) => {
-        const isShow =
-            b.title.includes(filter.title) &&
-            (!filter.discount || b.txtOff != null) &&
-            (!filter.cachback || b.isCashback) &&
-            (!filter.cachback2 || (b.isCashback && b.rate && b.rate >= 50));
-        b.ref.hidden = !isShow;
-    });
-};
-const filterApp = document.createElement("div");
-filterApp.className = "d-lcol";
-document
-    .querySelector("#main-bmk > div.d-area > div > div:last-of-type")
-    ?.prepend(filterApp);
-ReactDOM.render(<FilterComponnet onUpdate={handleFilter} />, filterApp);
-
-// CashbackDetail
-allItems
-    .filter((b) => b.isCashback)
-    .forEach((b) => {
-        const app = document.createElement("div");
-        b.ref.firstElementChild?.appendChild(app);
-        ReactDOM.render(<CashbackDetailComponent book={b} />, app);
-    });
+initializeFilter(allItems);
+initializeCacheDetila(allItems);
